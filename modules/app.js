2288808ea0153ea17d32ab06a26aeba46210fd45
a-sabagh <a.sabagh72@gmail.com>
2022-07-28 20:53:37 +0430

    modular programming default/named export

--- modules/app.js
+++ modules/app.js
@@ -1,1 +1,9 @@
 export const name = 'Abolfazl'
+const famil = 'sabagh'
+//export { famil }
+//export default 20
+
+var age = 20
+//export default age
+export default age
+export { famil }
--- modules/main.js
+++ modules/main.js
@@ -1,3 +1,9 @@
-import { name } from './app.js'
-
-console.log(name)
+/*import { name } from './app.js'
+import { famil } from './app.js'*/
+//import { name ,famil} from './app.js'
+//import {name as firstName,famil as lastName} from './app.js'
+//import * as app from './app.js'
+//import age from './app.js'
+//console.log(app.name,app.famil)
+import age , { famil } from './app.js'
+console.log(age,famil)
