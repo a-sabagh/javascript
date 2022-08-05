f802b1dcab203aaed03110b20c46f2884d366cee
a-sabagh <a.sabagh72@gmail.com>
2022-08-05 22:49:39 +0430

    import/export type of syntaxes 1659723579

--- modules/app.js
+++ modules/app.js
@@ -1,9 +1,29 @@
+/*
 export const name = 'Abolfazl'
-const famil = 'sabagh'
-//export { famil }
-//export default 20
+export const famil = 'sabagh'*/
 
+/*
+const name = 'Abolfazl'
+const famil = 'sabagh'
+export { name,famil }*/
+
+/*
 var age = 20
-//export default age
 export default age
-export { famil }
+
+/*
+export default 20 */
+
+/*
+export default function hello(){
+	console.log('hello')
+}*/
+
+/*
+var x = 10
+var y = 15
+function sum(x,y){
+	return x+y
+}
+
+export {sum as default,x,y} */
--- modules/main.js
+++ modules/main.js
@@ -1,9 +1,24 @@
-/*import { name } from './app.js'
-import { famil } from './app.js'*/
-//import { name ,famil} from './app.js'
-//import {name as firstName,famil as lastName} from './app.js'
-//import * as app from './app.js'
-//import age from './app.js'
-//console.log(app.name,app.famil)
-import age , { famil } from './app.js'
-console.log(age,famil)
+/*
+import { name } from './app.js'
+import { famil } from './app.js'
+
+import { name ,famil} from './app.js'*/
+
+/*
+import {name as firstName,famil as lastName} from './app.js'*/
+
+/*
+import * as app from './app.js'*/
+
+/*
+import age from './app.js'*/
+
+/*
+import age , { famil } from './app.js'*/
+
+/*
+import func from './app.js'*/
+
+/*
+import func , {x,y} from './app.js'
+console.log(func(x,y))*/
