bf59cf51893d3710f41abe56d2741c5b633588a1
a-sabagh <a.sabagh72@gmail.com>
2022-09-05 02:58:27 +0430

    request with fetch api based on promise structure

---
+++ fetch/server.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+echo json_encode([
+	'status' => true,
+	'data' => [
+		'message' => 'it work',
+	]
+]);
+
