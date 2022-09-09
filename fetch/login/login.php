965562f7f4edf0ac77469993a600d255e8d895b6
a-sabagh <a.sabagh72@gmail.com>
2022-09-10 00:01:53 +0430

    fetch and form data

---
+++ fetch/login/login.php
@@ -0,0 +1,25 @@
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if("admin" == $username && "12345" == $password){
+	echo json_encode([
+		'status' => true,
+		'data' => [
+			'requestBody' => $_POST,
+			'message' => 'Authorization is correct'
+		]
+	]);
+}else{
+	header("HTTP/1.1 401 Unauthorized");
+	echo json_encode([
+		'status' => false,
+		'statusCode' => 401,
+		'data' => [
+			'requestBody' => $_POST,
+			'message' => 'Unauthorized'
+		]
+	]);
+}
+
