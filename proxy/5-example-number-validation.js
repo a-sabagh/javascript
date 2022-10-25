4fca84f483f71ad39cea25a165dcf2236af359a4
a-sabagh <a.sabagh72@gmail.com>
2022-10-25 23:35:54 +0330

    example throw range error on invalid value in proxy

--- proxy/5-example-number-validation.js
+++ proxy/5-example-number-validation.js
@@ -1,9 +1,9 @@
 let handler = {
 	set(target,prop,value,proxy){
-		if('mount' == prop){
-			value = (value <= 12)? value : undefined	
-		} else if('day' == prop){
-			value = (value <= 31)? value : undefined	
+		if('mount' == prop && value > 12){
+			throw new RangeError('invalid value for Mount')
+		} else if('day' == prop && value > 31){
+			throw new RangeError('invalid value for Day')
 		}
 		target[prop] = value
 	}
